1807edb34e52fadd56907c58c3204feaca8d8323
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-11-24 23:25:54 +0100

    [master] Testfall fixed
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

--- src/main/java/com/github/funthomas424242/rezeptsammlung/rezept/Rezept.java
+++ src/main/java/com/github/funthomas424242/rezeptsammlung/rezept/Rezept.java
@@ -10,12 +10,12 @@ package com.github.funthomas424242.rezeptsammlung.rezept;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -35,5 +35,16 @@ public class Rezept {
 
     @Indexed(name = "name", type = "string")
     private String name;
+
+
+    public Rezept(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
 
 }
--- src/test/java/com/github/funthomas424242/rezeptsammlung/RezeptsammlungApplicationTests.java
+++ src/test/java/com/github/funthomas424242/rezeptsammlung/RezeptsammlungApplicationTests.java
@@ -10,26 +10,39 @@ package com.github.funthomas424242.rezeptsammlung;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
  * #L%
  */
 
+import com.github.funthomas424242.rezeptsammlung.rezept.Rezept;
+import com.github.funthomas424242.rezeptsammlung.rezept.RezeptRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RezeptsammlungApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    RezeptRepository rezeptRepository;
+
+    @Test
+    void contextLoads() {
+
+        Rezept rezept = new Rezept("P0001", "Phone");
+        rezeptRepository.save(rezept);
+
+        final Rezept retrievedProduct = rezeptRepository.findById("P0001").get();
+        retrievedProduct.setName("Smart Phone");
+        rezeptRepository.save(retrievedProduct);
+    }
 
 }
