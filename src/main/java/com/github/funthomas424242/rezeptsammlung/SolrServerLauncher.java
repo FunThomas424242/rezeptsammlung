fd5d010ea72df539bf1284df7670b8017fd67d8e
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-11-25 21:29:24 +0100

    [master] Quellen der Recherche über NOSQL embedded databases aufgenommen
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

--- src/main/java/com/github/funthomas424242/rezeptsammlung/SolrServerLauncher.java
+++ src/main/java/com/github/funthomas424242/rezeptsammlung/SolrServerLauncher.java
@@ -10,52 +10,49 @@ package com.github.funthomas424242.rezeptsammlung;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
  * #L%
  */
 
-
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.core.CoreContainer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.solr.server.support.EmbeddedSolrServerFactory;
-import org.springframework.data.solr.server.support.EmbeddedSolrServerFactoryBean;
 import org.xml.sax.SAXException;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-@SpringBootApplication
-public class RezeptsammlungApplication {
+public class SolrServerLauncher {
 
-    private static final Logger LOG = LoggerFactory.getLogger(RezeptsammlungApplication.class);
-
-    protected EmbeddedSolrServer embeddedSolrServer;
+    private EmbeddedSolrServer embeddedSolrServer;
     protected CoreContainer coreContainer;
 
     public static void main(String[] args) {
-        SpringApplication.run(RezeptsammlungApplication.class, args);
+        final SolrServerLauncher launcher = new SolrServerLauncher();
+        try {
+            launcher.startSolr();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
     }
 
-    @PostConstruct
-    protected void startSolrServer() throws ParserConfigurationException, SAXException, IOException {
-        LOG.info("### startSorlServer");
-        final Path path = Paths.get( "solrhome");
+    private void startSolr() throws ParserConfigurationException, IOException, SAXException {
+        //        LOG.info("### startSorlServer");
+        final Path path = Paths.get("solrhome");
 //        System.out.println("###"+path.toAbsolutePath().toString());
 //        System.out.println("###>"+path.resolve("solr.xml").toAbsolutePath().toString());
 //        coreContainer = CoreContainer.createAndLoad(path);
@@ -69,18 +66,6 @@ public class RezeptsammlungApplication {
         System.out.println("### start2");
         this.coreContainer = embeddedSolrServer.getCoreContainer();
         System.out.println("### start ended");
-        LOG.info("### startSorlServer geschafft");
+//        LOG.info("### startSorlServer geschafft");
     }
-
-    @PreDestroy
-    protected void shutdownSolrServer() {
-        LOG.info("### shutdownSorlServer");
-        embeddedSolrServer.getCoreContainer().shutdown();
-        if (!coreContainer.isShutDown()) {
-            // zoombie servers container
-            coreContainer.shutdown();
-        }
-        LOG.info("### shutdownSorlServer geschafft");
-    }
-
 }
