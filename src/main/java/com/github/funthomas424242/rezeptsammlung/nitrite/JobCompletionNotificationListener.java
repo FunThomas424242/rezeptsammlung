9fd25e2ea667d7e807d96a43e8f81e98746abc96
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-12-11 20:53:16 +0100

    [#10_sync] JobCompletionNotificationListener als Komponente realisiert
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

--- src/main/java/com/github/funthomas424242/rezeptsammlung/nitrite/JobCompletionNotificationListener.java
+++ src/main/java/com/github/funthomas424242/rezeptsammlung/nitrite/JobCompletionNotificationListener.java
@@ -29,8 +29,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class JobCompletionNotificationListener<T> extends JobExecutionListenerSupport {
 
     private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
