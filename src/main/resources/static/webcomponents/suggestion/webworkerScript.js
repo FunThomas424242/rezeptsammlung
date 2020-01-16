aa152614fe450e2530414f053b49a3cb29f59701
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2020-01-16 19:12:33 +0100

    [#19-thymeleaf] application über context path verschiebbar gestaltet, context path auf /rezeptsammlung gesetzt
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

--- src/main/resources/static/webcomponents/suggestion/webworkerScript.js
+++ src/main/resources/static/webcomponents/suggestion/webworkerScript.js
@@ -4,9 +4,9 @@ self.onmessage = function(e) {
   self.postMessage("msg from worker "+e.data);
   var jsonPromise;
   if( e.data ){
-    jsonPromise = self.fetch("http:localhost:8080/api/rezepte/tags?taglist="+e.data);
+    jsonPromise = self.fetch("http:localhost:8080/rezeptsammlung/api/rezepte/tags?taglist="+e.data);
   }else{
-    jsonPromise = self.fetch("http:localhost:8080/api/rezepte/tags");
+    jsonPromise = self.fetch("http:localhost:8080/rezeptsammlung/api/rezepte/tags");
   }
   jsonPromise.then( (response)=> {
     var promise = response.json();
@@ -22,7 +22,7 @@ self.onmessage = function(e) {
 function createSelectionContent( json ){
     var content = "";
     for (const key of Object.keys(json)) {
-        content += "<option value=\"""+key+"\">";
+        content += "<option value=\""+key+"\">";
     }
    return content;
 }
